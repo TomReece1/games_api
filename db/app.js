b552e96a2613fbe96efb60f8b5e78ce36f2dece8
Tom Reece <tomreece@toms-mbp.home>
2022-07-04 11:35:18 +0100

    add 404 and 500 error handlers

--- db/app.js
+++ db/app.js
@@ -15,4 +15,13 @@ app.use(express.json());
 
 app.get("/api/categories", getCategories);
 
+app.use("*", (req, res) => {
+  res.status(404).send({ msg: "Invalid Path" });
+});
+
+app.use((err, req, res, next) => {
+  console.log("in 500 handler");
+  res.status(500).send({ msg: "something went wrong" });
+});
+
 module.exports = app;
